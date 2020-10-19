70ca2235a96f60efb1ac5ea5ef7989ca6231f0bb
eorfeorf <orerrurirar2@gmail.com>
2020-10-20 04:44:38 +0900

    ノーツの生成を分離

---
+++ Assets/Ringtone/Notes/NotePrefabsScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ringtone.Notes
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/NotePrefabsScriptableObject", order = 1)]
+    public class NotePrefabsScriptableObject : ScriptableObject
+    {
+        public BaseNote[] Notes;
+    }
+}
